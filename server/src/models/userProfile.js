2647ec87a935cab2c0ec82c7d05d5896b023aa3b
Vinit Parakh <parakh.vinit2002@gmail.com>
2023-06-26 16:19:37 +0530

    minner changes

--- server/src/models/userProfile.js
+++ server/src/models/userProfile.js
@@ -1,24 +1,23 @@
 const mongoose = require('mongoose');
 
-
 const userProfileSchema = new mongoose.Schema({
-
-    attendence : {
-
-        date : {
-            type : Date,
-            required : true,
-            expires : '30d',
-        },
-    },
-    createdBy : {
-        type : String ,
-        required : true,
-    },
-    profilePhoto : {
-        type : String,
-        // required : true,
-    }
+  attendence: [
+    { [date]: { type: Date, required: true, expires: '30d' } },
+    //   date: {
+    //     type: Date,
+    //     required: true,
+    //     expires: '30d',
+    //   },
+    // },
+  ],
+  createdBy: {
+    type: String,
+    required: true,
+  },
+  profilePhoto: {
+    type: String,
+    // required : true,
+  },
 });
 
 module.exports = mongoose.model('UserProfile', userProfileSchema);
