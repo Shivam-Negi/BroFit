aea48af2432efa262e57af3563e9f424b2939a01
dashingvinit <92840061+dashingvinit@users.noreply.github.com>
2023-06-26 18:25:07 +0530

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Pages from './pages/Pages';
 import { BrowserRouter } from 'react-router-dom';
+import Pages from './pages/Pages';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Pages />
     </BrowserRouter>
   );
