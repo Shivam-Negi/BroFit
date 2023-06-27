8a6b7c29bf6f58b5d40c74e7002e3a57943fa3fd
Arjun <arjundeshta20@gmail.com>
2023-06-27 12:24:51 +0530

    reolved error

--- server/src/routes/v1/index.js
+++ server/src/routes/v1/index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const gymRouter = require('./gym-routes');
 const userRouter = require('./user-routes');
-const gymOwnerRouter = require('./gymOwner-routes');
-const userProfileRouter = require('./gymOwner-routes');
+
+const userProfileRouter = require('./userProfile-routes');
 const planRouter = require('./plan-routes');
 const authRouter = require('./authRoutes');
 
 router.use('/gym', gymRouter);
-router.use('/gymOwner', gymOwnerRouter);
 router.use('/user', userRouter);
 router.use('/userprofile', userProfileRouter);
 router.use('/plan', planRouter);
