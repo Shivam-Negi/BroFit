dd3dab8fd2823792bdb49a6f4dc054dffeb5395c
Shivam <shivamnegi478@gmail.com>
2023-07-24 07:36:14 +0530

    checking cron working v1

--- server/src/utils/common/cron-jobs.js
+++ server/src/utils/common/cron-jobs.js
@@ -17,7 +17,7 @@ async function graphCron() {
       const hour = currentTime.split(':')[0];
       const gyms = await gymRepository.getAll();
       for (const gym of gyms) {
-        console.log('gym inside cron:', gym);
+        // console.log('gym inside cron:', gym);
         let liveMem = gym.currentlyCheckedIn;
         // console.log(`liveMem of gymId ${gym.gymId} is ${liveMem}`);
         await gymRepository.updateByGymId(gym.gymId, {
@@ -40,7 +40,7 @@ async function checkOutCron() {
     cron.schedule('*/15 * * * * ', async () => {
       try {
         const members = await AttendanceService.getStatusInUsers(status);
-          console.log('members inside cron:', members);
+          // console.log('members inside cron:', members);
         const currentTime = HrsToMins(checkInTime());
         // console.log('time : ', currentTime);
         for(const member of members) {
@@ -102,17 +102,18 @@ async function planExCron() {
 
 // every night at 2 a.m. 
 async function graphResetCron() {
-  cron.schedule('0 2 * * *', async () => {
+  cron.schedule('*/5 * * * * *', async () => {
     console.log('resetting graph');
     try {
       const gyms = await gymRepository.getAll();
       for (const gym of gyms) {
         for(let i = 0; i < 24; ++i) {
-          await gymRepository.updateByGymId(gym.gymId, {
+          const res = await gymRepository.updateByGymId(gym.gymId, {
             $set: {
               [`liveGraph.${i}`]: 0,
             }
           });
+          console.log(`${gym} reset ${res}`);
         }
       }
     } catch (error) {
